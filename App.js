98e13f999202168ee49412fb6dbfdfb5746afe2b
Matheus M <matheus150101miranda@gmail.com>
2023-09-19 11:23:12 -0300

    criei o component 'Modal' e as funcionalidades de gerar senha e abrir e fechar  o modal

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import Slider from "@react-native-community/slider";
+import { useState } from "react";
+import ModalPassword from "./src/components/modal";
+
+const charset =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 export default function App() {
+  const [size, setSize] = useState(10);
+  const [passwordValue, setPasswordValue] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const generatePassword = () => {
+    let password = "";
+
+    for (let i = 0, n = charset.length; i < size; i++) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPasswordValue(password);
+
+    setModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./src/assets/logo.png")} style={styles.logo} />
+
+      <Text style={styles.title}>{size} Caracteres</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ minHeight: 50 }}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor={"#ff0000"}
+          minimumTrackTintColor={"#000"}
+          thumbTintColor={"#392de9"}
+          value={size}
+          onValueChange={(value) => setSize(value.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+        <ModalPassword
+          password={passwordValue}
+          handleClose={() => setModalVisible(false)}
+        />
+      </Modal>
     </View>
   );
 }
@@ -13,8 +66,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F3F3FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  area: {
+    width: "80%",
+    padding: 6,
+    borderRadius: 8,
+    marginVertical: 14,
+    backgroundColor: "#FFF",
+  },
+  button: {
+    width: "80%",
+    height: 50,
+    borderRadius: 8,
+    marginBottom: 18,
+    backgroundColor: "#392de9",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 20,
   },
 });
---
+++ src/components/Modal/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function ModalPassword({ password, handleClose }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Senha gerada</Text>
+
+        <Pressable style={styles.innerPassword}>
+          <Text style={styles.text}>{password}</Text>
+        </Pressable>
+
+        <View style={styles.buttonArea}>
+          <TouchableOpacity style={styles.button} onPress={handleClose}>
+            <Text style={styles.buttonText}>Voltar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button, styles.buttonSave]}>
+            <Text style={styles.buttonSaveText}>Salvar senha</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(24,24,24,0.6)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    backgroundColor: "#fff",
+    width: "85%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 24,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 24,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  innerPassword: {
+    backgroundColor: "#0e0e0e",
+    width: "90%",
+    padding: 14,
+    borderRadius: 8,
+  },
+  text: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  buttonArea: {
+    flexDirection: "row",
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 8,
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    marginVertical: 14,
+    padding: 8,
+  },
+  buttonSave: {
+    backgroundColor: "#392de9",
+    borderRadius: 8,
+  },
+  buttonSaveText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
